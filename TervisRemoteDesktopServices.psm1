8a25e1dea1098688ab5c5bbf118c09e35b167c88
Greg <gkjono@users.noreply.github.com>
2017-05-30 14:23:06 -0400

    Expanding Module

--- TervisRemoteDesktopServices.psm1
+++ TervisRemoteDesktopServices.psm1
@@ -3,4 +3,68 @@
         $EnvironmentName
     )
     Invoke-ClusterApplicationProvision -ClusterApplicationName RemoteWebBrowserApp -EnvironmentName $EnvironmentName
+    $Nodes = Get-TervisClusterApplicationNode -ClusterApplicationName RemoteWebBrowserApp -EnvironmentName $EnvironmentName
+    $Nodes | Add-TervisRdsServer
+    $Nodes | Add-TervisRdsSessionHost
+    $Nodes | New-TervisRdsSessionCollection
+}
+
+function Invoke-StoresRemoteDesktopProvision {
+    param (
+        $EnvironmentName
+    )
+    Invoke-ClusterApplicationProvision -ClusterApplicationName StoresRemoteDesktop -EnvironmentName $EnvironmentName
+    $Nodes = Get-TervisClusterApplicationNode -ClusterApplicationName StoresRemoteDesktop -EnvironmentName $EnvironmentName
+    $Nodes | New-TervisRdsSessionCollection
+    $Nodes | Add-TervisRdsSessionHost
+}
+
+function Add-TervisRdsServer {
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]$ComputerName
+    )
+    Begin {
+        $RDBroker = Get-ADComputer -filter 'Name -like "*broker*"' | Select -ExpandProperty DNSHostName
+        $DNSRoot = Get-ADDomain | Select -ExpandProperty DNSRoot
+    }
+    Process {
+        $SessionHost = ($Node).ComputerName + '.' + $DNSRoot
+        If (-NOT (Get-RDServer -ConnectionBroker $RDBroker -Role RDS-RD-SERVER -ErrorAction SilentlyContinue | Where Server -Contains $SessionHost)) {
+            Add-RDServer -Server $SessionHost -ConnectionBroker $RDBroker -Role RDS-RD-SERVER
+        }
+    }
+}
+
+function New-TervisRdsSessionCollection {
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]$ComputerName,
+        [Parameter(ValueFromPipelineByPropertyName)]$ClusterApplicationName
+    )
+    Begin {
+        $RDBroker = Get-ADComputer -filter 'Name -like "*broker*"' | Select -ExpandProperty DNSHostName
+        $DNSRoot = Get-ADDomain | Select -ExpandProperty DNSRoot
+    }
+    Process {
+        If (-NOT (Get-RDSessionCollection -ConnectionBroker $RDBroker -CollectionName ($Node).ClusterApplicationName -ErrorAction SilentlyContinue)) {
+            $SessionHost = ($Node).ComputerName + '.' + $DNSRoot
+            New-RDSessionCollection -CollectionName ($Node).ClusterApplicationName -ConnectionBroker $RDBroker -SessionHost $SessionHost -CollectionDescription 'Stores Remote Desktop Services' 
+        }
+    }
+}
+
+function Add-TervisRdsSessionHost {
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]$ComputerName,
+        [Parameter(ValueFromPipelineByPropertyName)]$ClusterApplicationName
+    )
+    Begin {
+        $RDBroker = Get-ADComputer -filter 'Name -like "*broker*"' | Select -ExpandProperty DNSHostName
+        $DNSRoot = Get-ADDomain | Select -ExpandProperty DNSRoot
+    }
+    Process {
+        If (-NOT ((Get-RDSessionHost -CollectionName ($Node).ClusterApplicationName -ConnectionBroker $RDBroker -ErrorAction SilentlyContinue) -contains ($Node).ComputerName)) {
+            $SessionHost = ($Node).ComputerName + '.' + $DNSRoot
+            Add-RDSessionHost -CollectionName ($Node).ClusterApplicationName -SessionHost $SessionHost -ConnectionBroker $RDBroker
+        }
+    }
 }
