3ca908bd6869b147b80dfba726054d3ea4abfc26
hambone124 <hpe60m5@gmail.com>
2017-07-19 16:48:46 -0400

    Install-HelixDowntimeClient

--- TervisRemoteDesktopServices.psm1
+++ TervisRemoteDesktopServices.psm1
@@ -504,3 +504,30 @@ powershell -windowstyle hidden -noprofile -Command "\\$DomainName\applications\P
     }
 }
 
+function Install-HelixDowntimeClient {
+    param (
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ComputerName
+    )
+    begin {
+        $HelixDowntimeRemoteApp = "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\RemoteApp and Desktop Connections\Work Resources\Helix Downtime Client (Work Resources).lnk"
+    }
+    process {
+        Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+            $UserProfiles = Get-ChildItem -Path C:\Users
+            foreach ($UserProfile in $UserProfiles) {
+                $HelixDowntimeRemoteAppFullPath = Join-Path -Path $UserProfile.FullName -ChildPath $Using:HelixDowntimeRemoteApp
+                Write-Verbose "Looking for $HelixDowntimeRemoteAppFullPath"
+                Write-Verbose "Found status: $(Test-Path $HelixDowntimeRemoteAppFullPath)"
+                if (Test-Path -Path $HelixDowntimeRemoteAppFullPath) {
+                    $RemoteAppSource = $HelixDowntimeRemoteAppFullPath
+                    break
+                }
+            }
+            if ($RemoteAppSource) {
+                Copy-Item -Path $RemoteAppSource -Destination C:\Users\Public\Desktop -Force 
+            } else {
+                Write-Warning "No RemoteApps found on $env:COMPUTERNAME"
+            }
+        }
+    }
+}
